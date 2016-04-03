1c465aaab5e6b074e9378daa147a18e56253c4fb
gabelut10 <gabelut10@gmail.com>
2016-04-03 17:29:29 +0300

    Adding main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: gabi
+ *
+ * Created on April 3, 2016, 5:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
